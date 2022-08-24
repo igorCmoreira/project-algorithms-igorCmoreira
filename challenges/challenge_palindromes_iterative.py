6821f9eceefc4f697beebb250cebe18a9e3ab2c5
Igor Campos Moreira <igorcampos200@gmail.com>
2022-08-24 01:44:58 -0300

    Requisito 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,5 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    letters = list(word)
+    return letters == [letter for letter in word[::-1]]
