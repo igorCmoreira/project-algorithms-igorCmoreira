9782a88319ab685b41a5771da9e03f615bad6269
Igor Campos Moreira <igorcampos200@gmail.com>
2022-08-24 01:10:28 -0300

    test req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,12 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui.  """
+    if len(first_string) != len(second_string):
+        return False
+    counter = 0
+    first_string = first_string.lower()
+    second_string = second_string.lower()
+    for letter1 in first_string:
+        for letter2 in second_string:
+            if letter1 == letter2:
+                counter += 1
+
+    return counter == len(first_string)
